ee89fab0dc4ca3b748dd366e951e67f4e540e1b3
LULU SEVIGNON <LUDIVINE.SEVIGNON@GMAIL.COM>
2021-04-14 10:19:28 -0700

    edit and delete button styled

--- src/components/Post.js
+++ src/components/Post.js
@@ -2,18 +2,19 @@ import React from "react";
 import { Link } from "react-router-dom";
 import { gql } from 'apollo-boost';
 import { useMutation } from '@apollo/react-hooks'
+import './post.css'
 
-const classes = {
-  container: "bg-white border rounded-lg overflow-hidden",
-  tr: "odd:bg-gray-100 even:bg-white border-t",
-  td: "font-semibold p-3 text-right md:w-1/5",
-  edit:
-    "text-xs bg-blue-600 text-white hover:bg-blue-700 rounded px-4 py-2 uppercase font-semibold tracking-wide",
-  delete:
-    "text-xs bg-red-600 text-white hover:bg-red-700 rounded px-4 py-2 uppercase font-semibold tracking-wide",
-  li: "inline-block ml-2",
-  nav: "my-4 mx-2 text-center"
-};
+// const classes = {
+//   container: "bg-white border rounded-lg overflow-hidden",
+//   tr: "odd:bg-gray-100 even:bg-white border-t",
+//   td: "font-semibold p-3 text-right md:w-1/5",
+//   edit:
+//     "text-xs bg-blue-600 text-white hover:bg-blue-700 rounded px-4 py-2 uppercase font-semibold tracking-wide",
+//   delete:
+//     "text-xs bg-red-600 text-white hover:bg-red-700 rounded px-4 py-2 uppercase font-semibold tracking-wide",
+//   li: "inline-block ml-2",
+//   nav: "my-4 mx-2 text-center"
+// };
 
 const DELETE_POST = gql`
 mutation DeletePost($id: uuid!) {
@@ -36,37 +37,37 @@ function Post( { post, refetch }) {
 
   return (
     <>
-      <div className={classes.container}>
+      <div className="container">
         <table className="w-full text-sm">
           <tbody>
-            <tr className={classes.tr}>
-              <td className={classes.td}>id</td>
+            <tr className="tr">
+              <td className="td">id</td>
               <td className="p-3">{post.id}</td>
             </tr>
-            <tr className={classes.tr}>
-              <td className={classes.td}>title</td>
+            <tr className="tr">
+              <td className="td">title</td>
               <td className="p-3">{post.title}</td>
             </tr>
-            <tr className={classes.tr}>
-              <td className={classes.td}>body</td>
+            <tr className="tr">
+              <td className="td">body</td>
               <td className="p-3">{post.body}</td>
             </tr>
-            <tr className={classes.tr}>
-              <td className={classes.td}>createdAt</td>
+            <tr className="tr">
+              <td className="td">createdAt</td>
               <td className="p-3">{post.createdAt}</td>
             </tr>
           </tbody>
         </table>
       </div>
-      <nav className={classes.nav}>
+      <nav className="nav">
         <ul>
-          <li className={classes.li}>
-            <Link to={`/edit/${post.id}`} className={classes.edit}>
+          <li className="li">
+            <Link to={`/edit/${post.id}`} className="edit">
               Edit
             </Link>
           </li>
-          <li className={classes.li}>
-            <button onClick={() => handleDeletePost(post.id)} className={classes.delete}>Delete</button>
+          <li className="li">
+            <button onClick={() => handleDeletePost(post.id)} className="delete">Delete</button>
           </li>
         </ul>
       </nav>
